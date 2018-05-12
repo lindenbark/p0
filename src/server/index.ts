6d47108188df8ea252c0c0fd9eeba2b3c25d3ef6
JongChan Choi <jong@chan.moe>
2018-05-12 20:54:12 +0900

    ws

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,13 @@
-console.log('hello, world!');
+import * as WebSocket from 'ws';
+
+const wss = new WebSocket.Server({ port: 10001 });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        for (let client of wss.clients) {
+            if (ws === client) continue;
+            console.log(message);
+        }
+    });
+    ws.send(JSON.stringify({ type: 'ok' }));
+});
